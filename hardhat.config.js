04e4ef78348ad2866d9cabe628ccd17f423c2a25
vswaroop04 <vswaroop04@gmail.com>
2023-03-07 20:23:20 +0530

    aadded tasks,deploy and gas fee

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,18 @@
 require('@nomicfoundation/hardhat-toolbox')
 require('dotenv').config()
+require('@nomiclabs/hardhat-etherscan')
+require('./tasks/block-number')
+require('hardhat-gas-reporter')
+require('solidity-coverage')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY } = process.env
+const {
+  ALCHEMY_API_KEY,
+  GOERLI_PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+  COIN_MARKET_CAP_API_KEY,
+} = process.env
 
 console.log(`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`)
 
@@ -17,4 +26,15 @@ module.exports = {
     },
   },
   solidity: '0.8.0',
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    nocolour: true,
+    currency: 'INR',
+    coinmarketcap: COIN_MARKET_CAP_API_KEY,
+    token: 'MATIC',
+  },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,6 @@
-const { ethers } = require('hardhat')
+const { ethers, run, network } = require('hardhat')
+require('@nomiclabs/hardhat-etherscan')
+
 async function main() {
   const SimpleStorage = await ethers.getContractFactory('SimpleStorage')
 
@@ -7,6 +9,37 @@ async function main() {
   await simpleStorage.deployed()
 
   console.log('SimpleStorage deployed to:', simpleStorage.address)
+  console.log(process.env.ETHERSCAN_API_KEY)
+  console.log(network.config.chainId)
+  if (process.env.ETHERSCAN_API_KEY && network.config.chainId === 5) {
+    await simpleStorage.deployTransaction.wait(5)
+    await verify(simpleStorage.address, [])
+  }
+
+  const value = simpleStorage.retrieve()
+  console.log('Value:', value)
+
+  console.log('Setting value to 5...')
+  const transresponse = await simpleStorage.store(5)
+  transresponse.wait(1)
+  const setvalue = simpleStorage.retrieve()
+  console.log(`Value set to ${setvalue}`)
+}
+
+async function verify(contractaddress, args) {
+  console.log('Verifying Contract ....')
+  try {
+    await run('verify:verify', {
+      address: contractaddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Contract already verified')
+    } else {
+      console.log(e)
+    }
+  }
 }
 
 main()
